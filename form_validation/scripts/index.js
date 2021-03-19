be263855e5d48975df4ac95166eee2182bd59413
Misha2048 <mikaelan.itsmart@gmail.com>
2021-03-19 09:39:51 +0200

    form_validation

---
+++ form_validation/scripts/index.js
@@ -0,0 +1,65 @@
+$(document).ready(function() {
+	$("input[name='done']").bind("click", function() {
+		//setup error fields
+		let username_error = $("span[name='username_error']");
+		let age_error = $("span[name='age_error']");
+		let password_error = $("span[name='password_error']");
+		let gender_error = $("span[name='gender_error']");
+		let message_error = $("span[name='message_error']");
+		username_error.text("");
+		age_error.text("");
+		password_error.text("");
+		gender_error.text("");
+		message_error.text("");
+
+
+		//getting userdata
+		let username = $("input[name='username']").val();
+		let age = $("input[name='age']").val();
+		let password = $("input[name='password']").val();
+		let gender = $("input[name='gender']:checked").val();
+		let remme = $("input[name='remember_me']");
+		let town = $("select[name='town']").val();
+		let message = $("textarea[name='message']").val();
+
+		let data_block = $("div#userdata");
+
+		let error = false; //to check form validation
+
+		//form validation
+		if(!username) {
+			error = true;
+			username_error.text("Enter username, please");
+		}
+		if(!age) {
+			error = true;
+			age_error.text("Enter your age, please");
+		}
+		if(!password) {
+			error = true;
+			password_error.text("Enter password, please");
+		}
+		if(!gender) {
+			error = true;
+			gender_error.text("Choose gender, please");
+		}
+		if(!message) {
+			error = true;
+			message_error.text("Enter message, please");
+		}
+
+		//if data validated
+		if(!error) {
+			data_block.empty();
+			data_block.append("Data validated:<br/>");
+			data_block.append("Username: " + username + "<br/>");
+			data_block.append("Age: " + age + "<br/>");
+			data_block.append("Password " + password + "<br/>");
+			data_block.append("Gender: " + gender + "<br/>");
+			data_block.append("Remember me: " + remme.is(":checked") + "<br/>");
+			data_block.append("Town: " + town + "<br/>");
+			data_block.append("Message " + message + "<br/>");
+		}
+
+	});
+});
